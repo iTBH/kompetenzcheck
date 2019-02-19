0d3b40f9e80907d45450f65dd15587bc39605dde
Lukas Ebbers <lukas.ebbers@wabsolute.de>
2019-02-19 09:35:44 +0100

    - diverse Designanpassungen

--- resources/views/checks/_form.blade.php
+++ resources/views/checks/_form.blade.php
@@ -1,20 +1,23 @@
 <div class="fields">
     <div class="sixteen wide field">
-        <label for="title">Kurztitel des Checks: <i class="help circle outline icon" data-content="Wie lautet der Arbeitsauftrag, der dem Check zugrunde liegt? Vergeben Sie einen möglichst prägnanten Check-Titel."></i></label>
+        <label for="title">Kurztitel des Checks: <i class="help circle outline icon"
+                                                    data-content="Wie lautet der Arbeitsauftrag, der dem Check zugrunde liegt? Vergeben Sie einen möglichst prägnanten Check-Titel."></i></label>
         <input type="text" name="title" id="title" value="{{ old('title', $check->title) }}"/>
     </div>
 </div>
 
 <div class="fields">
     <div class="sixteen wide field">
-        <label for="purpose">Verwendungszweck: <i class="help circle outline icon" data-content="Mit welchem Ziel wird der Check durchgeführt? z.B. Kompetenzentwicklung in einer Veranstaltung."></i></label>
+        <label for="purpose">Verwendungszweck: <i class="help circle outline icon"
+                                                  data-content="Mit welchem Ziel wird der Check durchgeführt? z.B. Kompetenzentwicklung in einer Veranstaltung."></i></label>
         <input type="text" name="purpose" id="purpose" value="{{ old('purpose', $check->purpose) }}"/>
     </div>
 </div>
 
 <div class="fields">
     <div class="sixteen wide field">
-        <label for="description">Auftragsbeschreibung: <i class="help circle outline icon" data-content="Beschreiben Sie den Arbeitsauftrag ausführlich:{{-- Rahmenbedingungen (z.B. Mitarbeiter, Ort, Zeitraum), Anforderungen/Vorschriften, Arbeitsgegenstände, zu erstellende (Teil)Produkte.--}} Es muss deutlich werden, in welchem Zusammenhang die Kompetenzebeschreiung gecheckt werden soll."></i></label>
+        <label for="description">Auftragsbeschreibung: <i class="help circle outline icon"
+                                                          data-content="Beschreiben Sie den Arbeitsauftrag ausführlich:{{-- Rahmenbedingungen (z.B. Mitarbeiter, Ort, Zeitraum), Anforderungen/Vorschriften, Arbeitsgegenstände, zu erstellende (Teil)Produkte.--}} Es muss deutlich werden, in welchem Zusammenhang die Kompetenzebeschreiung gecheckt werden soll."></i></label>
         <textarea name="description" id="description">{{ old('description', $check->description) }}</textarea>
     </div>
 </div>
@@ -22,7 +25,7 @@
 <div class="field">
     <label for="tags">Schlagwörter</label>
     <div id="tags" class="ui fluid multiple search selection dropdown" onkeypress="return event.keyCode != 13;">
-        <input name="tags" type="hidden" value="{{ $check ? implode(',', $check->tags->pluck('name')->toArray()) : old('tags') }}">
+        <input name="tags" type="hidden" value="{{ $check->id ? implode(',', $check->tags->pluck('name')->toArray()) : old('tags') }}">
         <i class="dropdown icon"></i>
         <div class="default text">Hinzufügen</div>
         <div class="menu">
@@ -35,34 +38,50 @@
 
 <div class="ui divider"></div>
 <h2 class="green colored">Kompetenzbeschreibungen</h2>
-<?php $tabs = ['1' => ' active', '2' => '', '3' => '', '4' => '' ]; ?>
-<div class="ui top attached tabular menu">
+<div class="tabular-menu">
+    <?php $tabs = ['1' => ' active', '2' => '', '3' => '', '4' => '']; ?>
+    <div class="ui top attached tabular menu">
+        @foreach( $tabs as $_index => $_class )
+            <a class="item{{ $_class }} one-fourth" data-tab="{{ $_index  }}">
+                <input value="@isset($phases[$_index]['phase']){{ $phases[$_index]['phase']->name }}@else{{old('phase')[$_index]}}@endisset" name="phase[{{$_index}}]"
+                       placeholder="Bereich {{ $_index }}">
+            </a>
+        @endforeach
+    </div>
+    <?php $phrases = old('phrase'); ?>
     @foreach( $tabs as $_index => $_class )
-        <a class="item{{ $_class }} one-fourth" data-tab="{{ $_index  }}">
-            <input value="@isset($phases[$_index]['phase']){{ $phases[$_index]['phase']->name }}@endisset" name="phase[{{$_index}}]">
-        </a>
+        <div class="ui bottom attached{{ $_class }} tab segment" data-tab="{{ $_index }}">
+            <a href="javascript:void(0)" class="ui icon add-phrase-btn plus">
+                +
+            </a>
+            <a href="javascript:void(0)" class="ui small icon button add-phrase-btn">
+                <i class="plus square outline icon"></i> Kompetenzbeschreibung hinzufügen
+            </a>
+            @isset( $phases[$_index]['phrases'] )
+                @forelse ($phases[$_index]['phrases'] as $_phrase)
+                    @include('checks.phrase.item', [
+                        'statement' => $_phrase->statement,
+                        'category' => $_phrase->getCategory(),
+                        'tab' => $_index
+                    ])
+                @empty
+                    <br/><br/>
+                    <strong>Hinweis:</strong> Fügen Sie an dieser Stellen neue Kompetenzbeschreibungen hinzu. Klicken Sie dafür auf den Plus-Button in der jeweiligen Prozessphase.
+                @endforelse
+            @elseif($phrases)
+                @foreach( $phrases as $_phrase )
+                    @if($_phrase['tab'] == $_index)
+                        @include('checks.phrase.item', [
+                            'statement' => $_phrase['statement'],
+                            'category' => (new \App\Models\Category)->find($_phrase['category']),
+                            'tab' => $_index
+                        ])
+                    @endif
+                @endforeach
+            @endisset
+        </div>
     @endforeach
 </div>
-@foreach( $tabs as $_index => $_class )
-    <div class="ui bottom attached{{ $_class }} tab segment" data-tab="{{ $_index }}">
-        <a href="javascript:void(0)" class="ui small icon button add-phrase-btn">
-            <i class="plus square outline icon"></i> Kompetenzbeschreibung hinzufügen
-        </a>
-        @isset( $phases[$_index]['phrases'] )
-            @forelse ($phases[$_index]['phrases'] as $_phrase)
-                @include('checks.phrase.item', [
-                    'statement' => $_phrase->statement,
-                    'category' => $_phrase->getCategory(),
-                    'tab' => $_index
-                ])
-            @empty
-                <br /><br />
-                <strong>Hinweis:</strong> Fügen Sie an dieser Stellen neue Kompetenzbeschreibungen hinzu. Klicken Sie dafür auf den Plus-Button in der jeweiligen Prozessphase.
-            @endforelse
-        @endisset
-    </div>
-@endforeach
-
 <div class="ui divider"></div>
 <div class="fields">
     <div class="sixteen wide field">
