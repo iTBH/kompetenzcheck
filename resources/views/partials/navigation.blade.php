57862924a211c922e099205ebc7263eb6e022df8
Lukas Ebbers <lukas.ebbers@wabsolute.de>
2019-03-11 08:39:46 +0100

    Fix - No isAdmin function

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -4,9 +4,6 @@
         <div><a href="{{ route('dashboard.index') }}" class="{{ Request::is('dashboard*') || Request::is('check*') ? 'active' : null }}">Checks</a></div>
         <div><a href="{{ route('partner.index') }}" class="{{ Request::is('partner*') ? 'active' : null }}">Partner/innen</a></div>
         <div><a href="{{ route('tags.index') }}" class="{{ Request::is('tags*') ? 'active' : null }}">Schlagwörter</a></div>
-        @if(Auth::user()->isAdmin())
-            <div><a href="{{ route('admin.users.index') }}" class="{{ Request::is('admin*') ? 'active' : null }}">Administration</a></div>
-        @endif
 
         <!-- topnavigation - responsive -->
         <div><a href="{{ route('profile.index') }}" class="{{ Request::is('profile*') ? 'active' : null }} topnavigation">{{ Auth::user()->name }}</a></div>
