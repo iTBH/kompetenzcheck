70ae3f540a46df2e4b23ac7ba83017ee2a944380
Lukas <lukas.ebbers@wabsolute.de>
2019-05-22 14:48:13 +0200

    Add variable to dd()

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -25,7 +25,7 @@ class EvaluationController extends Controller
             $name = str_replace(' ', '_', $check->name) . "_" . Carbon::now()->format('d_m_Y');
             return $pdf->download($name . '.pdf');
         } catch (\Exception $exception) {
-            dd($exception, $css);
+            dd($exception, $url, env('APP_URL'));
             session()->flash('status', ['message' => 'Das PDF kann gerade nicht erstellt werden', 'level' => 'error']);
             return redirect()->back();
         }
