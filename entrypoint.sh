aa8bf5713b135d226c6fc93d64a1fda456927e10
Andreas Böttger <andreas.boettger@tuhh.de>
2019-05-23 07:58:16 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,6 @@ if [ ! -f /var/www/html/.env ]; then
     echo "APP_URL=${APP_URL}" >> /var/www/html/.env
 fi
 
-# `/var/www/html/storage/app/public/` ist ein Verzeichnis, welches beim Starten
-# des Containers in den Container "gemountet" wird. Die Berechtigungen für
-# dieses Verzeichnis müssen korrigiert werden
-chown -R www-data:www-data /var/www/html/storage/app/public/
 
 php artisan key:generate
 php artisan storage:link
@@ -21,6 +17,9 @@ php artisan migrate
 php artisan db:seed
 php artisan cache:clear
 php artisan view:clear
+
+
+find /var/www/html/ ! -user www-data -exec chown www-data:www-data {} \
 
 php-fpm &
 caddy
