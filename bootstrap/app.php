0e3c204237c70e66fdef8e02b080f4db12f2797c
Andreas Böttger <andreas.boettger@tuhh.de>
2019-05-23 11:46:42 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,11 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$json = new \Monolog\Formatter\JsonFormatter();
+$stdouthandler = new \Monolog\Handler\StreamHandler('php://stdout', 'info');
+$stdouthandler->setFormatter($json);
+Log::pushHandler($stdouthandler);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
